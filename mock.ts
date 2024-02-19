301bae42853331a0f30c40773cd6c714be848997
WyLToR <bekesi.patrik@gmail.com>
2024-02-19 11:19:44 +0100

    mock data

---
+++ mock.ts
@@ -0,0 +1,298 @@
+const data = {
+  users: [
+    {
+      "email": "teszt@example.com",
+      "username": "teszt.elek",
+      "password": "12341234",
+      "firstName": "Teszt",
+      "lastName": "Elek",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2001-01-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    },
+    {
+      "email": "peterpan@example.com",
+      "username": "panpeter",
+      "password": "password123",
+      "firstName": "Peter",
+      "lastName": "Pan",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2002-01-01T00:00:00.000Z",
+      "isAdmin": true,
+      "permissions": []
+    },
+    {
+      "email": "alice@example.com",
+      "username": "alicejohn",
+      "password": "qwerty",
+      "firstName": "Alice",
+      "lastName": "Johnson",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2003-01-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    },
+    {
+      "email": "bob@example.com",
+      "username": "bobsmith",
+      "password": "asdf1234",
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2004-01-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    },
+    {
+      "email": "admin@admin.com",
+      "username": "WyLToR",
+      "password": "admin",
+      "firstName": "Patrik",
+      "lastName": "Békési",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2005-01-01T00:00:00.000Z",
+      "isAdmin": true,
+      "permissions": []
+    },
+    {
+      "email": "johndoe@example.com",
+      "username": "johndoe",
+      "password": "password123",
+      "firstName": "John",
+      "lastName": "Doe",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2006-01-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    },
+    {
+      "email": "janedoe@example.com",
+      "username": "janedoe",
+      "password": "password456",
+      "firstName": "Jane",
+      "lastName": "Doe",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2007-01-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    },
+    {
+      "email": "charliebrown@example.com",
+      "username": "charlie.brown",
+      "password": "password789",
+      "firstName": "Charlie",
+      "lastName": "Brown",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2008-01-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    },
+    {
+      "email": "lucyvanpelt@example.com",
+      "username": "lucyvan",
+      "password": "passwordABC",
+      "firstName": "Lucy",
+      "lastName": "Van Pelt",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2000-02-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    },
+    {
+      "email": "snoopy@example.com",
+      "username": "snoopydog",
+      "password": "passwordXYZ",
+      "firstName": "Snoopy",
+      "lastName": "dog",
+      "img": "https://placehold.co/600x600",
+      "createdAt": "2020-03-01T00:00:00.000Z",
+      "isAdmin": false,
+      "permissions": []
+    }
+  ],
+  pages: [
+    {
+      "pageId": "YrxLkGjS6N",
+      "pageName": "Első oldal",
+      "pageHref": "",
+      "pageVisible": true,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    },
+    {
+      "pageId": "R5kfdD0sfa",
+      "pageName": "Második oldal",
+      "pageHref": "",
+      "pageVisible": false,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    },
+    {
+      "pageId": "a7Hf93Hdj2",
+      "pageName": "Harmadik oldal",
+      "pageHref": "",
+      "pageVisible": false,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    },
+    {
+      "pageId": "9h2Dm7ksL1",
+      "pageName": "Negyedik oldal",
+      "pageHref": "",
+      "pageVisible": false,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    },
+    {
+      "pageId": "rfgjKm3Rt2",
+      "pageName": "Ötödik oldal",
+      "pageHref": "",
+      "pageVisible": true,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    },
+    {
+      "pageId": "yDjLmKslR5",
+      "pageName": "Hatodik oldal",
+      "pageHref": "",
+      "pageVisible": false,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    },
+    {
+      "pageId": "fsaNj9D3Hj",
+      "pageName": "Hetedik oldal",
+      "pageHref": "",
+      "pageVisible": true,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    },
+    {
+      "pageId": "oJd9Lkfa7H",
+      "pageName": "Nyolcadik oldal",
+      "pageHref": "",
+      "pageVisible": true,
+      "createdAt": "2000-01-01T00:00:00.000Z"
+    }
+  ],
+  permissions: [
+    {
+      "permissionId": "bo98DfkjP7",
+      "permissionName": "Első jogosultsági csoport",
+      "permissionDetails": "",
+      "createdAt": "2001-01-01T00:00:00.000Z"
+    },
+    {
+      "permissionId": "akf67BhfK3",
+      "permissionName": "Második jogosultsági csoport",
+      "permissionDetails": "",
+      "createdAt": "2002-01-01T00:00:00.000Z"
+    },
+    {
+      "permissionId": "pfgJd2Fkh8",
+      "permissionName": "Harmadik jogosultsági csoport",
+      "permissionDetails": "",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "permissionId": "sG3jD4l0f9",
+      "permissionName": "Negyedik jogosultsági csoport",
+      "permissionDetails": "",
+      "createdAt": "2004-01-01T00:00:00.000Z"
+    }
+  ],
+  categories: [
+    {
+      "categoryId": "6L1Km7h9Df",
+      "categoryName": "Első kategória",
+      "categoryDetail": "",
+      "createdAt": "2005-01-01T00:00:00.000Z"
+    },
+    {
+      "categoryId": "3HdjLmKf92",
+      "categoryName": "Második kategória",
+      "categoryDetail": "",
+      "createdAt": "2010-01-01T00:00:00.000Z"
+    },
+    {
+      "categoryId": "7HjP9Df8k3",
+      "categoryName": "Harmadik kategória",
+      "categoryDetail": "",
+      "createdAt": "2022-01-01T00:00:00.000Z"
+    }
+  ],
+  gallery: [
+    {
+      "pictureId": "Bm7n9DjfKo",
+      "pictureName": "Első fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/400/200",
+      "createdAt": "2001-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "jfKo9DBm7n",
+      "pictureName": "Második fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/500/350",
+      "createdAt": "2002-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "7n9DjfKoBm",
+      "pictureName": "Harmadik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/510/390",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "5gHp8DjeMq",
+      "pictureName": "Negyedik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/440/270",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "9a9b1D1e2f",
+      "pictureName": "Ötödik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/450/320",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "oP2l0Kj3Hn",
+      "pictureName": "Hatodik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/600/500",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "uYr7tHg9Fp",
+      "pictureName": "Hetedik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/630/530",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "eF2d4Vg7Bh",
+      "pictureName": "Nyolcadik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/520/320",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "pL5m2Iq9As",
+      "pictureName": "Kilencedik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/430/330",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "cP4z8Mx6Vb",
+      "pictureName": "Tizedik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/550/450",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    },
+    {
+      "pictureId": "qW9r5Ae3Gs",
+      "pictureName": "Tizenegyedik fénykép",
+      "pictureAlt": "Fénykép alt",
+      "pictureHref": "https://picsum.photos/600/400",
+      "createdAt": "2003-01-01T00:00:00.000Z"
+    }
+  ]
+}
+export default data;
