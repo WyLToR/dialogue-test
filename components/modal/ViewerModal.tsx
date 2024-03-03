dd5ddd05fa9894e0fce1c3a5fdc2e36763491bf7
WyLToR <bekesi.patrik@gmail.com>
2024-03-03 15:38:09 +0100

    img paginator

--- components/modal/ViewerModal.tsx
+++ components/modal/ViewerModal.tsx
@@ -1,48 +1,98 @@
-import { useEffect } from "react";
+import Picture from "@/src/interfaces/picture";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import ReactModal from "react-modal";
 
 export default function ViewerModal({
   modal,
   setModal,
-  children,
+  galleryData,
+  actualImg,
+  setActualImg,
 }: {
   modal: boolean;
   setModal: React.Dispatch<React.SetStateAction<boolean>>;
-  children: React.ReactNode;
+  galleryData: Picture[];
+  actualImg: number;
+  setActualImg: React.Dispatch<React.SetStateAction<number>>;
 }) {
   const { t } = useTranslation();
+  const [currentItemIndex, setCurrentItemIndex] = useState(actualImg);
+  const handlePrev = () => {
+    setCurrentItemIndex((prevIndex) => Math.max(0, prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentItemIndex((prevIndex) =>
+      Math.min(galleryData.length - 1, prevIndex + 1)
+    );
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: { key: string }) => {
+      if (event.key === "Escape") {
+        setModal(false);
+        setActualImg(0);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [modal, setModal]);
 
   return (
     <>
-      <div className="relative flex justify-center items-center bg-inherit">
+      <div className="relative flex justify-center items-center h-full">
         <ReactModal
           shouldCloseOnEsc={true}
           isOpen={modal}
           ariaHideApp={false}
-          overlayClassName={{
-            base: "fixed inset-0 bg-opacity-50 bg-black",
-            afterOpen: "opacity-100",
-            beforeClose: "opacity-0",
-          }}
           className={{
-            base: "flex h-full items-center justify-center flex-col",
-            afterOpen:
-              "opacity-100 transition-all duration-500 ease-in-out transform translate-z-0",
-            beforeClose: "opacity-0 -translate-x-full duration-300",
+            base: "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300 dark:bg-gray-800 opacity-90 rounded-lg p-8",
+            afterOpen: "opacity-100 transition-all duration-500 ease-in-out",
+            beforeClose: "opacity-0 duration-300",
           }}
+          onRequestClose={() => setModal(!modal)}
+          shouldCloseOnOverlayClick={true}
         >
           <div className="flex justify-center items-center text-center">
-            {children}
+            <img
+              src={galleryData[currentItemIndex].pictureHref}
+              alt="picture"
+            />
           </div>
-          <button
-            onClick={() => setModal(!modal)}
-            className={`${
-              modal ? "block" : "none"
-            } mx-auto mt-10 rounded-xl border-4 border-transparent bg-blue-400 px-6 py-3 text-center text-base font-medium text-black-100`}
-          >
-            {t("modalSuccessViewerClose")}
-          </button>
+          <div className="flex items-center justify-between mt-3">
+            <button
+              onClick={handlePrev}
+              className={`${
+                modal ? "block" : "none"
+              } mx-auto rounded-xl border-4 border-transparent bg-blue-600 px-6 py-3 text-center text-base font-medium text-black`}
+            >
+              {"<"}
+            </button>
+            <button
+              onClick={() => {
+                setModal(!modal);
+                setActualImg(0);
+              }}
+              className={`${
+                modal ? "block" : "none"
+              } mx-auto mt-3 rounded-xl border-4 border-transparent bg-blue-600 px-6 py-3 text-center text-base font-medium text-black`}
+            >
+              {t("modalSuccessViewerClose")}
+            </button>
+            <button
+              onClick={handleNext}
+              className={`${
+                modal ? "block" : "none"
+              } mx-auto rounded-xl border-4 border-transparent bg-blue-600 px-6 py-3 text-center text-base font-medium text-black`}
+            >
+              {">"}
+            </button>
+          </div>
         </ReactModal>
       </div>
     </>
